467dd6eff881e6de9a50301bd28dbd7eabf7c8e6
Adrian <adrianlazocabrera@gmail.com>
2025-12-09 19:24:55 -0500

    add: profile adrian

---
+++ src/app/features/adrian-profile/pages/adrian-profile/adrian-profile.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adrian-profile',
+  imports: [],
+  templateUrl: './adrian-profile.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AdrianProfile { }
